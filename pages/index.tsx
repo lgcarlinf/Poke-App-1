3b2d066eb5aac76b161e4f1df99d4f7d993b64e4
luiggy <usuario@MacBook-Air-de-Usuario.local>
2022-05-08 11:55:27 -0500

    meta tag fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const Home: NextPage<Props> = ({ pokemons }) => {
   return (
-    <Layout>
+    <Layout title="Poke App">
       <Grid.Container gap={2} justify="flex-start">
         {pokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
